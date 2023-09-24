e4765309566ca3fdad63bbd3e4c36a05009051db
gregory-code <gregrigdon@icloud.com>
2023-09-24 00:26:45 -0500

    Music and Animations
    You know how "his music was electric"? Well my music is code... Not nearly as cool

--- Assets/_MyAssets/Scripts/menuScript.cs
+++ Assets/_MyAssets/Scripts/menuScript.cs
@@ -5,11 +5,16 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Rendering;
+using UnityEngine.Audio;
 using System.Linq;
 
 public class menuScript : MonoBehaviour, IDataPersistence
 {
     notifScript NotificationScript;
+
+    //PlayerPref
+    private float _musicLevel;
+    private float _effectsLevel;
 
     //Firebase Data
     FirebaseScript fireBaseScript;
@@ -21,7 +26,7 @@ public class menuScript : MonoBehaviour, IDataPersistence
     [SerializeField] private Sprite[] _background_Sprites;
     [SerializeField] private Sprite[] _button_Sprites;
 
-    [Header("Variables")]
+    [Header("Cloud Variables")]
     public TMP_InputField UsernameField;
     public TMP_InputField KillsField;
 
@@ -29,11 +34,28 @@ public class menuScript : MonoBehaviour, IDataPersistence
     private int _fieldInt;
     private bool _bInt;
 
+    [Header("Animations")]
+    [SerializeField] TMP_Text titleText;
+    private string currentMenu;
+    private Animator menuAnimations;
+
     [Header("Menus Lerp")]
     [SerializeField] float friendSpeed = 6;
     private bool _bFriends;
     private Transform _friends;
     private Transform _friendsIcon;
+
+    [Header("Audio")]
+    [SerializeField] Image muteIcon;
+    [SerializeField] Sprite mutedSprite;
+    [SerializeField] Sprite unmutedSprite;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider effectsSlider;
+    [SerializeField] AudioMixer MasterMixer;
+    [SerializeField] AudioSource testEffect;
+    private bool bReuglarMusic;
+    [SerializeField] AudioMixerSnapshot Regular;
+    [SerializeField] AudioMixerSnapshot Muted;
 
     private void Awake()
     {
@@ -44,11 +66,24 @@ public class menuScript : MonoBehaviour, IDataPersistence
         OnlineScript = GameObject.FindGameObjectWithTag("Online").GetComponent<onlineScript>();
 
         NotificationScript = GameObject.FindGameObjectWithTag("Canvas").GetComponent<notifScript>();
+
+        menuAnimations = GetComponent<Animator>();
     }
 
     private void Start()
     {
         menuStyle(Random.Range(0, _background_Sprites.Length)); // picks a random style at game start
+
+        //Setting music
+        _musicLevel = PlayerPrefs.GetFloat("musicLevel");
+        _effectsLevel = PlayerPrefs.GetFloat("effectsLevel");
+
+        if (_musicLevel == 0) _musicLevel = 1;
+        if (_effectsLevel == 0) _effectsLevel = 1;
+
+        setMusicLevel(_musicLevel);
+        setEffectsLevel(_effectsLevel);
+        // ========
     }
 
     private void Update()
@@ -59,6 +94,8 @@ public class menuScript : MonoBehaviour, IDataPersistence
         float friendsIconLerp = (_bFriends) ? -187 : -360;
         _friendsIcon.localPosition = Vector2.Lerp(_friendsIcon.localPosition, new Vector2(friendsIconLerp, 234), friendSpeed * Time.deltaTime);
     }
+
+    #region menuing
 
     private void menuStyle(int change)
     {
@@ -75,6 +112,86 @@ public class menuScript : MonoBehaviour, IDataPersistence
     {
         _bFriends = !_bFriends;
     }
+
+    public void OpenMenu(string name)
+    {
+        if (currentMenu == name) return;
+
+        menuAnimations.SetTrigger("scatter");
+
+        currentMenu = name;
+
+        menuAnimations.SetBool("openBuilder", false);
+        menuAnimations.SetBool("openBattle", false);
+        menuAnimations.SetBool("openMusic", false);
+        menuAnimations.SetBool("openProfile", false);
+        menuAnimations.SetBool("open" + name, true);
+    }
+
+    public void returnMenu()
+    {
+        menuAnimations.ResetTrigger("scatter");
+        menuAnimations.SetTrigger("leave" + currentMenu);
+    }
+
+    public void setTextToName()
+    {
+        titleText.text = currentMenu;
+    }
+
+    public void setTextToMain()
+    {
+        titleText.text = "Main Menu";
+        menuAnimations.ResetTrigger("leave" + currentMenu);
+        currentMenu = "";
+    }
+
+    #endregion
+
+    #region Song Menu
+
+    public void setMusicLevel(float sliderValue)
+    {
+        PlayerPrefs.SetFloat("musicLevel", sliderValue);
+        MasterMixer.SetFloat("musicVol", Mathf.Log10(sliderValue) * 20);
+        _musicLevel = sliderValue;
+        musicSlider.value = sliderValue;
+        checkMute();
+    }
+
+    public void setEffectsLevel(float sliderValue)
+    {
+        PlayerPrefs.SetFloat("effectsLevel", sliderValue);
+        MasterMixer.SetFloat("effectsVol", Mathf.Log10(sliderValue) * 20);
+        _effectsLevel = sliderValue;
+        effectsSlider.value = sliderValue;
+        testEffect.Play();
+        
+        checkMute();
+    }
+
+    private void checkMute()
+    {
+        if (muteIcon.sprite == mutedSprite) muteIcon.sprite = unmutedSprite;
+        if (_effectsLevel == 0.0001f && _musicLevel == 0.0001f) muteIcon.sprite = mutedSprite;
+    }
+
+    public void mute()
+    {
+        if (muteIcon.sprite == mutedSprite)
+        {
+            setMusicLevel(1f);
+            setEffectsLevel(1f);
+
+        }
+        else // unmutted
+        {
+            setMusicLevel(0.0001f);
+            setEffectsLevel(0.0001f);
+        }
+    }
+
+    #endregion
 
     public void ReadStringInput(string s)
     {
